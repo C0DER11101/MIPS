eb0c9315b42752ec216ccc67eb723e914aa4085e
Priyanuj Bora <while.alive.program@gmail.com>
2023-06-08 14:14:23 +0530

    Getting user input

---
+++ tests/asm11.asm
@@ -0,0 +1,22 @@
+.data
+	msg: .asciiz "Enter a floating point number: "
+	output: .asciiz "Number is: "
+	Zero: .float 0.0
+
+.text
+	lwc1 $f2, Zero
+	
+	li $v0, 4
+	la $a0, msg
+	syscall
+	
+	li $v0, 6
+	syscall  # the input entered by the user will be a float which will be stored in $f0 register.
+	
+	li $v0, 4
+	la $a0, output
+	syscall
+	
+	li $v0, 2
+	add.s $f12, $f0, $f2
+	syscall

e6dba53d0a4a518d7cedb886c7687a55667434e1
Priyanuj Bora <while.alive.program@gmail.com>
2023-06-08 17:25:00 +0530

    slt(set less than)

---
+++ tests/asm15.asm
@@ -0,0 +1,24 @@
+.data
+	message: .asciiz "Number is less than the other"
+
+.text
+
+	main:
+		li $t0, 1
+		li $t1, 5
+
+		slt $t3, $t0, $t1 # if t1 < t2 then t3=1(true) otherwise t3=0(false)
+
+		bne $t3, $0, Greater
+
+
+		li $v0, 10
+		syscall
+	
+	Greater:
+		li $v0, 4
+		la $a0, message
+		syscall
+
+		li $v0, 10
+		syscall

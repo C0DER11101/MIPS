1ebfb7ce620f07c01c1cf51fa67ba9f881c5f31d
Priyanuj Bora <while.alive.program@gmail.com>
2023-06-09 06:53:44 +0530

    blt(branch if less than)

---
+++ tests/asm16B.asm
@@ -0,0 +1,27 @@
+.data
+	msg1: .asciiz "Hello"
+	msg2: .asciiz "Nothing"
+
+.text
+
+	main:
+		li $s0, 10
+		li $s1, 14
+
+		blt $s0, $s1, sayHello # if s0<s1 then goto sayHello
+		
+		li $v0, 4
+		la $a0, msg2
+		syscall
+
+		li $v0, 10
+		syscall
+
+	sayHello:
+		li $v0, 4
+		la $a0, msg1
+		syscall
+
+		li $v0, 10
+		syscall
+
